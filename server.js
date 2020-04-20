10c3c146e50313da6f3893b55209adc3fb0916ac
liviocavalcantid@gmail.com <62245330+fiteclsouza@users.noreply.github.com>
2020-04-20 15:47:02 -0300

    update

--- server.js
+++ server.js
@@ -1,31 +1,10 @@
-const express = require('express');
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-const favicon = require('express-favicon');
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-const path = require('path');
-
-const port = process.env.PORT || 8080;
-
-const app = express();
-
-app.use(favicon(__dirname + '/build/favicon.ico'));
-
-// the __dirname is the current directory from where the script is running
-
-app.use(express.static(__dirname));
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', function (req, res) {
-
- return res.send('pong');
-
-});
-
-app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-
-});
-
-app.listen(port);
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
